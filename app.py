b3d7d8cb5bed99cb8ad333ad369e9575395ef857
Abeed Chowdhury <abeed.azaac@gmail.com>
2024-12-30 14:59:53 -0500

    add functionality to download csv file with preditions. add table to show last days predicitons and have input feilds for user predictions

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import plotly.graph_objs as go
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output, State
 import Class
+import io
 
 
 start_date = "2024-12-2"
@@ -24,8 +25,41 @@ actual.create_feature([{"Feature": "NYISpjm shock X forecast"},{"Feature": "PJMn
 
 app = Dash(__name__)
 app.layout = html.Div([
+    
     dcc.Graph(id='how-shocky'),
     dcc.Graph(id='model-spread'),
+    html.Button("Download Data", id="btn-download"),
+    dcc.Download(id="download-dataframe-csv"),
+    html.Table([
+        # Header row (Hours 1-24)
+        html.Tr([html.Th('Hour')] + [html.Th(str(i)) for i in range(0, 24)]),
+        
+        # NYIS pjm DA row
+        html.Tr(
+            [html.Td('NYIS pjm DA')] + 
+            [html.Td(id=f'nyis-da-{i}') for i in range(0, 24)]
+        ),
+        
+        # NYISpjm shock X forecast row
+        html.Tr(
+            [html.Td('NYISpjm shock X forecast')] + 
+            [html.Td(id=f'nyis-shock-{i}') for i in range(0, 24)]
+        ),
+        
+        # User input row
+        html.Tr(
+            [html.Td('User Bids')] + 
+            [html.Td(dcc.Input(
+                id=f'user-input-{i}',
+                type='number',
+                style={'width': '50px'}
+            )) for i in range(0, 24)]
+        )
+    ], style={
+        'border-collapse': 'collapse',
+        'width': '100%',
+        'textAlign': 'center'
+    })
 ])
 
 @app.callback(
@@ -88,6 +122,22 @@ def update_how_shocky(id):
     return fig
 
 @app.callback(
+    Output("download-dataframe-csv", "data"),
+    Input("btn-download", "n_clicks"),
+    prevent_initial_call=True,
+)
+def func(n_clicks):
+    # Get the last day of data
+    last_day = actual.df.index.max().date()
+    df_last_day = actual.df[actual.df.index.date == last_day]
+    
+    # Select and rename columns
+    df_to_download = df_last_day[["NYIS pjm DA", "NYISpjm shock X forecast"]].reset_index()
+    df_to_download = df_to_download.rename(columns={"datetime": "Datetime (HB)"})
+    
+    return dcc.send_data_frame(df_to_download.to_csv, f"nyis_pjm_data_{last_day}.csv", index=False)
+
+@app.callback(
     Output('model-spread', 'figure'),
     Input('model-spread', 'id'),
 )
@@ -139,6 +189,24 @@ def update_shock_models_predicted_spread(id):
     )
 
     return fig
+
+
+@app.callback(
+    [Output(f'nyis-da-{i}', 'children') for i in range(0, 24)] +
+    [Output(f'nyis-shock-{i}', 'children') for i in range(0, 24)],
+    Input('how-shocky', 'id')
+)
+def update_table(id):
+    last_day = actual.df.index.max().date()
+    df_last_day = actual.df[actual.df.index.date == last_day]
+    
+    da_values = df_last_day['NYIS pjm DA'].round(2).tolist()
+    shock_values = df_last_day['NYISpjm shock X forecast'].round(2).tolist()
+    
+    return da_values + shock_values
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
